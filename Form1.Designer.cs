65b6eaad0eb48022d5d431a88fe12f0225a55237
Noctade <endeade80@gmail.com>
2025-08-16 14:20:54 +0200

    Initial commit.

---
+++ Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace DSEDL
+{
+    partial class DefaultWindow
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DefaultWindow));
+            locationBrowse = new FolderBrowserDialog();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            label3 = new Label();
+            label4 = new Label();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            label5 = new Label();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(67, 114);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(264, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(379, 30);
+            label1.TabIndex = 1;
+            label1.Text = "Discord Sticker and Emoji Downloader";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(12, 39);
+            label2.Name = "label2";
+            label2.Size = new Size(407, 21);
+            label2.TabIndex = 2;
+            label2.Text = "Step 1: Select the type of emoticon you want to download";
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(118, 63);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(65, 24);
+            radioButton1.TabIndex = 3;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Emoji";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(203, 63);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(71, 24);
+            radioButton2.TabIndex = 4;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Sticker";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(12, 90);
+            label3.Name = "label3";
+            label3.Size = new Size(288, 21);
+            label3.TabIndex = 5;
+            label3.Text = "Step 2: Input the ID of the Emoji / Sticker";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(12, 144);
+            label4.Name = "label4";
+            label4.Size = new Size(434, 21);
+            label4.TabIndex = 6;
+            label4.Text = "Step 3: Put in the location where you want the file to be saved";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(36, 168);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(264, 27);
+            textBox2.TabIndex = 7;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(306, 168);
+            button1.Name = "button1";
+            button1.Size = new Size(101, 27);
+            button1.TabIndex = 8;
+            button1.Text = "Browse ...";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(12, 198);
+            label5.Name = "label5";
+            label5.Size = new Size(136, 21);
+            label5.TabIndex = 9;
+            label5.Text = "Step 4: Download!";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(157, 228);
+            button2.Name = "button2";
+            button2.Size = new Size(101, 27);
+            button2.TabIndex = 10;
+            button2.Text = "Download!";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // DefaultWindow
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            BackColor = SystemColors.ControlLightLight;
+            ClientSize = new Size(456, 366);
+            Controls.Add(button2);
+            Controls.Add(label5);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(3, 4, 3, 4);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "DefaultWindow";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "DSEDL";
+            Load += DefaultWindow_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private FolderBrowserDialog locationBrowse;
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox2;
+        private Button button1;
+        private Label label5;
+        private Button button2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Net;
+using System.Security.Policy;
+using System.Windows.Forms;
+namespace DSEDL
+{
+    public partial class DefaultWindow : Form
+    {
+        public DefaultWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DefaultWindow_Load(object sender, EventArgs e)
+        {
+            textBox2.Text = locationBrowse.SelectedPath;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            locationBrowse.ShowDialog();
+            textBox2.Text = locationBrowse.SelectedPath;
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            string id = textBox1.Text;
+            string folder = textBox2.Text;
+            string location = folder + "\\" + id + ".png";
+            if (radioButton1.Checked)
+            {
+                string link = "https://cdn.discordapp.com/emojis/" + id + ".png";
+                using (HttpClient client = new HttpClient())
+                {
+                    try
+                    {
+                        byte[] fileBytes = await client.GetByteArrayAsync(link);
+                        await File.WriteAllBytesAsync(location, fileBytes);
+                        MessageBox.Show("Successful.");
+                    } catch
+                    {
+                        MessageBox.Show("Failed.");
+                    }
+                }
+            }
+            if (radioButton2.Checked)
+            {
+                string link = "https://cdn.discordapp.com/stickers/" + id + ".png";
+                using (HttpClient client = new HttpClient())
+                {
+                    try
+                    {
+                        byte[] fileBytes = await client.GetByteArrayAsync(link);
+                        await File.WriteAllBytesAsync(location, fileBytes);
+                        MessageBox.Show("Successful.");
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Failed.");
+                    }
+                }
+            }
+
+        }
+    }
+}
